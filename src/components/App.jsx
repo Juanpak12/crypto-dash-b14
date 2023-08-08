d8c87e82fddede1764a8487578d96b46cae477b5
Juanpak12 <juanpak12@gmail.com>
2023-08-08 17:53:38 -0500

    se comenta App y Card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,13 @@
+// se importa la hoja de estilos
+
 import '../styles/App.css';
+
+// se importan elementos de react
+
 import { useState, useEffect, useCallback } from 'react';
+
+// se importan componentes
+
 import CardPrincipal from '../components/CardPrincipal';
 import Card from './Card';
 import Header from './Header';
@@ -7,10 +15,19 @@ import TableCoins from './TableCoins';
 import Convert from './Convert';
 import Footer from './Footer';
 
+// define el componente principal llamado app
 function App() {
+
+  //Se declara tres estados utilizando useState:
+  //coins: Almacena datos de las criptomonedas.
+  //currencys: Almacena las monedas admitidas para conversión.
+  //selCur: Almacena la moneda seleccionada para la conversión (por defecto, 'usd').
+
   const [coins, setCoins] = useState();
   const [currencys, setCurrencys] = useState();
   const [selCur, setSelCur] = useState('usd');
+
+  //Define la función getData, que utiliza useCallback para garantizar que la función no se vuelva a crear en cada renderizado. Esta función realiza llamadas a la API de CoinGecko para obtener datos de las criptomonedas y las monedas admitidas.
 
   const getData = useCallback(async () => {
     const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${selCur}&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d%2C1y`);
@@ -21,6 +38,7 @@ function App() {
     setCurrencys(curJson);
   }, [selCur]);
 
+//Utiliza useEffect para ejecutar la función getData cuando el componente se monta ([]) y cuando selCur cambia.
   useEffect(() => {
     getData();
   }, [getData]);
@@ -29,12 +47,21 @@ function App() {
     getData();
   }, [selCur]);
 
+  // Renderiza la interfaz del componente:
+  //Muestra un mensaje de "Cargando..." si coins es false (aún no se ha cargado la información).
+  
   return !coins ? 'Cargando...' : (
+
+    //Si coins tiene datos, muestra varios componentes personalizados, como Header, Convert, main (contiene tarjetas y un componente principal), TableCoins y Footer.
+
     <>
       <Header currencys={currencys} fun={setSelCur} cur={selCur} />
       <Convert />
       <main>
         <div className="cards_con">
+
+          {/*En el bloque coins.map dentro de la sección main, se mapean los datos de las criptomonedas para crear tarjetas Card para cada una de las cuatro primeras criptomonedas excepto la primera). */} 
+
           {coins.map(({ id, symbol, image, price_change_percentage_30d_in_currency, current_price }, index) => {
             if (index !== 0 && index <= 3) {
               return (
@@ -62,12 +89,19 @@ function App() {
 
 export default App;
 
+// El método deleteDec se utiliza para formatear el porcentaje de cambio. El bloque else retorna null para omitir las criptomonedas restantes.
+// funciones exportadas
+// deleteDec: Una función que toma un valor numérico y un número de decimales, y formatea el valor a un número con la cantidad de decimales especificada.
+
 export function deleteDec(val, decimal) {
   return val.toFixed(decimal);
 }
+
+//colorDec: Una función que toma un número y devuelve 'green' si es positivo y 'red' si es negativo.
 
 export function colorDec(num) {
   return num > 0 ? 'green' : 'red';
 }
 
+//numberF: Una instancia de Intl.NumberFormat configurada para formatear números en el formato específico 'es-ES'.
 export const numberF = Intl.NumberFormat('es-ES');
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,46 @@
+// se importa la hoja de estilos
 import "../styles/Card.css"
+
+// se importa el componente Graph
+
 import Graph from "./Graph"
+
+// se importa la función colorDec del archivo App.js. Esta función es utilizada para determinar el color del texto en función de si el porcentaje de cambio es positivo o negativo.
+
 import {colorDec} from './App'
 
+//La función Card toma varios props como argumentos: coinId, cur, porcentaje, price e img.
+
 export default function Card({coinId, cur, porcentaje, price, img}){
+
+    // se renderiza el contenido de la tarjeta
+
     return (
+
+        // Muestra una imagen de la criptomoneda (img) en la parte superior de la tarjeta.
+
         <div className="card">
             <img src={img} alt=""/>
+
+             {/*Dentro de div con la clase con-main, muestra el título de la tarjeta y el componente Graph.*/}
+
             <div className="con-main">
+
+                {/*El título incluye*/}
+
                 <div className="con-title">
+                    
+                    {/*Un h2 con la clase price, que muestra el precio actual de la criptomoneda (price). La clase colorDec(porcentaje) se utiliza para determinar el color del texto en función del porcentaje de cambio utilizando la función colorDec. */}
+
                     <h2 className={`price ${colorDec(porcentaje)}`}>{price}</h2>
+
+                    {/*Un h4 con la clase porcentajes, que muestra el porcentaje de cambio (porcentaje) seguido del símbolo de porcentaje (%). Al igual que antes, la clase colorDec(porcentaje) determina el color del texto basado en el porcentaje de cambio. */}
+
                     <h4 className={`porcentajes ${colorDec(porcentaje)}`}>{porcentaje}%</h4>
                 </div>
+                
+                {/*se incluye el componente Graph con los props coin, currency y color, que representan la criptomoneda, la moneda actual y el color correspondiente. */}
+
                 <Graph coin={coinId} currency={cur} color={colorDec(porcentaje)}/>
             </div>
         </div>
